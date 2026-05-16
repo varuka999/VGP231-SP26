9a8b4f38374f2dcc4bc0029eee895ae263c362bd
varuka999 <vjkpokemon@yahoo.com>
2026-05-16 10:31:55 -0700

    Scenerio-1 Combat Cycle

--- VGP231_VA/Assets/Scripts/Player/PlayerCombatHandler.cs
+++ VGP231_VA/Assets/Scripts/Player/PlayerCombatHandler.cs
@@ -1,14 +1,32 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerCombatHandler : MonoBehaviour
 {
+    [Header("Health")]
     private int healthCounter = 0;
     public int HealthCounter => healthCounter;
+    public string sceneName = string.Empty;
+
+    [Header("Damage VFX")]
+    public SpriteRenderer playerSprite = null;
+    public float damageFlashDuration = 0.15f;
+
+    private Color originalColor = Color.white;
+    private Tween damageFlashTween = null;
+
+    private void Awake()
+    {
+        if (playerSprite != null)
+        {
+            originalColor = playerSprite.color;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Attack")
+        if (other.gameObject.CompareTag("Attack"))
         {
             other.gameObject.GetComponent<AttackCollision>().DeactivateCollision();
             TakeDamage();
@@ -23,12 +41,30 @@ public class PlayerCombatHandler : MonoBehaviour
     private void TakeDamage()
     {
         --healthCounter;
+
+        PlayDamageFlash();
+
         // play sfx & audio
-        // check whether dead
+
         if (healthCounter <= 0)
         {
             PlayerDeath();
         }
+    }
+
+    private void PlayDamageFlash()
+    {
+        if (playerSprite == null)
+        {
+            Debug.Log("Sprite Not Found");
+            return;
+        }
+
+        damageFlashTween?.Kill();
+
+        playerSprite.color = Color.red;
+
+        damageFlashTween = playerSprite.DOColor(originalColor, damageFlashDuration).SetEase(Ease.OutQuad);
     }
 
     private void PlayerDeath()
@@ -36,6 +72,6 @@ public class PlayerCombatHandler : MonoBehaviour
         // play sfx & audio
         // prompt retry
         // temp
-        SceneManager.LoadScene("Scenario-Test");
+        SceneManager.LoadScene(sceneName);
     }
 }
