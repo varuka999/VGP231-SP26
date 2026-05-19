2ba82f347d38e951ddefebcdd257e8a709126221
varuka999 <vjkpokemon@yahoo.com>
2026-05-19 04:33:25 -0700

    IsInEncounter added to EnemyCombatHandler
    IsInEncounter is the boolean you want to refer to to know that combat is over.
    
    its set to true during the first combat cycle (so not immediately during the first dialogue of an encounter, only once the first combat cycle begins)
    
    its set to false only in CombatCycleEnd when all the attack cycles have been exhausted from that enemy (the function itself is called as the last event in a Zone's animation clip).

--- VGP231_VA/Assets/Scripts/Enemy/EnemyCombatHandler.cs
+++ VGP231_VA/Assets/Scripts/Enemy/EnemyCombatHandler.cs
@@ -5,10 +5,12 @@ public class EnemyCombatHandler : MonoBehaviour
 {
     [SerializeField] private List<GameObject> attackCycleGameObject = new List<GameObject>();
     private int combatIndex = 0;
-    private bool inCombat = false;
+    private bool inCombat = false; // this is only refering to if the player is currently in an active attack cycle, not if the player is in combat with the enemy in general
+    private bool isInEncounter = false; // this is refering to if the player is in combat with the enemy in general, not if the player is currently in an active attack cycle
     private float radiusStart = 0;
-
+    public bool IsInEncounter => isInEncounter;
     private PlayerCombatHandler playerCombatHandler = null;
+
     private void Start()
     {
         playerCombatHandler = FindAnyObjectByType<PlayerCombatHandler>();
@@ -21,6 +23,7 @@ public class EnemyCombatHandler : MonoBehaviour
         Debug.Log($"Combat Start!");
         if (combatIndex == 0)
         {
+            isInEncounter = true;
             playerCombatHandler.CombatStart();
 
             SphereCollider interactCollider = this.gameObject.GetComponent<SphereCollider>();
@@ -40,6 +43,11 @@ public class EnemyCombatHandler : MonoBehaviour
     public void CombatCycleEnd()
     {
         inCombat = false;
+        if (combatIndex >= attackCycleGameObject.Count)
+        {
+            isInEncounter = false;
+        }
+
         this.gameObject.GetComponent<DialogueSystem>().NextDialogue();
     }
 
